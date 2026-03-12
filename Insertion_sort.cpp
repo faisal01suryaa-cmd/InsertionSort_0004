d0361b670fc45bb200aba5cf0c261c9b96b89dcc
faisal01suryaa <faisal01suryaa@gmail.com>
2026-03-12 15:21:28 +0700

    menampilkan display bertuliskan elemen array

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -56,3 +56,17 @@ void insertionsort() //procedure insertion sort
         }
     }
 }
+
+void display()
+{
+    cout << endl;
+    cout << "\n=================";
+    cout << "\nElemen Array" << endl;
+    cout << "\n=================" << endl;
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
